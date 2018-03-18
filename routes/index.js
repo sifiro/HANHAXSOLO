307afcd481f01648b7ca3bc5e4fb24c852095837
Sifi <sifiro@outlook.com>
2018-03-18 10:41:47 +0100

    Some Improvements for works as Standalone and Proxy Forward Reverse (without rewrite support)

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,48 @@ global.IPAddress="";
 global.Package="";
 global.Table={};
 
+/* Standalone */
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 router.get('/index', function(req, res, next) {
+  res.render('index');
+});
+/* PS3Xplot 3.0 HAN, Statics Files */
+router.get('/enabler', function(req, res) {
+  res.sendFile('han_enabler.html', { root: path.join(__dirname, '../html')});
+});
+router.get('/installer', function(req, res) {
+  res.sendFile('han_installer.html', { root: path.join(__dirname, '../html')});
+});
+router.get('/dumper', function(req, res) {
+  res.sendFile('actidps_dumper.html', { root: path.join(__dirname, '../html')});
+});
+router.get('/debugpkg', function(req, res) {
+  res.sendFile('dbgpkg_enabler.html', { root: path.join(__dirname, '../html')});
+});
+/* PS3Xplot 3.0 HAN, Dynamics Files */
+router.get('/injecter', function(req, res) {
+  res.render('actrif_copier',{rifname:global.Table[global.Package]});
+});
+/**
+ *  Configs
+ */
+router.get('/config', require('./backend/config.js').GetterConfiguration);
+
+router.post('/config', require('./backend/config.js').SetterConfiguration);
+
+router.get('/table', require('./backend/config.js').GetterTable);
+
+router.post('/table', require('./backend/config.js').SetterTable);
+
+/* For Stranges Proxy Forwards */
+/* GET home page. */
+router.get(hanprefix+'/', function(req, res, next) {
+  res.render('index');
+});
+router.get(hanprefix+'/index', function(req, res, next) {
   res.render('index');
 });
 /* PS3Xplot 3.0 HAN, Statics Files */
