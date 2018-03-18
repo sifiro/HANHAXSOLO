b4c90443fde8d40db307835a242db4555f54fdf3
Sifi <sifiro@outlook.com>
2018-03-18 10:36:34 +0100

    Fix some issues for some proxy forwards like lighttpd

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var path = require("path");
 
+/// TODO: Load configuration from an JSON Local
+configprefix="/config";
+hanprefix="/han";
 global.IPAddress="";
 global.Package="";
 global.Table={};
@@ -14,32 +17,32 @@ router.get('/index', function(req, res, next) {
   res.render('index');
 });
 /* PS3Xplot 3.0 HAN, Statics Files */
-router.get('/enabler', function(req, res) {
+router.get(hanprefix+'/enabler', function(req, res) {
   res.sendFile('han_enabler.html', { root: path.join(__dirname, '../html')});
 });
-router.get('/installer', function(req, res) {
+router.get(hanprefix+'/installer', function(req, res) {
   res.sendFile('han_installer.html', { root: path.join(__dirname, '../html')});
 });
-router.get('/dumper', function(req, res) {
+router.get(hanprefix+'/dumper', function(req, res) {
   res.sendFile('actidps_dumper.html', { root: path.join(__dirname, '../html')});
 });
-router.get('/debugpkg', function(req, res) {
+router.get(hanprefix+'/debugpkg', function(req, res) {
   res.sendFile('dbgpkg_enabler.html', { root: path.join(__dirname, '../html')});
 });
 /* PS3Xplot 3.0 HAN, Dynamics Files */
-router.get('/injecter', function(req, res) {
+router.get(hanprefix+'/injecter', function(req, res) {
   res.render('actrif_copier',{rifname:global.Table[global.Package]});
 });
 /**
  *  Configs
  */
-router.get('/config', require('./backend/config.js').GetterConfiguration);
+router.get(configprefix+'/config', require('./backend/config.js').GetterConfiguration);
 
-router.post('/config', require('./backend/config.js').SetterConfiguration);
+router.post(configprefix+'/config', require('./backend/config.js').SetterConfiguration);
 
-router.get('/table', require('./backend/config.js').GetterTable);
+router.get(configprefix+'/table', require('./backend/config.js').GetterTable);
 
-router.post('/table', require('./backend/config.js').SetterTable);
+router.post(configprefix+'/table', require('./backend/config.js').SetterTable);
 
 
 module.exports = router;
